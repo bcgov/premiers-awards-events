02cc4bfbf25c71780607334d9c574bd022ce3e50
Spencer Rose <Spencer.Rose@gov.bc.ca>
2022-10-15 10:39:39 -0700

    Update build config.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { fileURLToPath, URL } from 'url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: "/events",
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+return defineConfig({
+    plugins: [vue()],
+    base: "/events/",
+    envDir: "/app",
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
-  }
-})
+  });
+
