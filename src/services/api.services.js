bee04d7427cda429abbd573d3f161a9279352292
Spencer Rose <Spencer.Rose@gov.bc.ca>
2022-10-15 16:48:29 -0700

    Update build config.

--- src/services/api.services.js
+++ src/services/api.services.js
@@ -8,7 +8,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
     dataType: "json",
