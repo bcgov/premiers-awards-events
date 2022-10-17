21297e566261b54c99aaa5147d71fd6e75ba9d63
Spencer Rose <Spencer.Rose@gov.bc.ca>
2022-10-17 12:24:06 -0700

    Update build config.

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to 'PA_APPS_' to load all PA web app env.
   const env = loadEnv(mode, process.cwd(), 'PA_APPS_');
+  console.log(env)
   return {
     envPrefix: 'PA_APPS_',
     plugins: [vue()],
