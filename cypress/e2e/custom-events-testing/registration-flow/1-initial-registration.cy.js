c42a9dcea20f3d246e5e91bc7ec46ac31677ad03
Bryson Best <103070659+brysonjbest@users.noreply.github.com>
2022-11-04 16:20:42 -0700

    Add initial registration and registration page testing.

--- cypress/e2e/custom-events-testing/registration-flow/1-initial-registration.cy.js
+++ cypress/e2e/custom-events-testing/registration-flow/1-initial-registration.cy.js
@@ -1,23 +1,92 @@
 /// <reference types="cypress" />
 const url = Cypress.env("url");
-const user = Cypress.env("user");
-require("dotenv").config();
-("use strict");
+import { getCustomLogin } from "../../helpers/login";
+import {
+  getSingleRegistration,
+  postRegistrations,
+  postSingleRegistration,
+} from "../../helpers/registrations";
 
-// initialization settings
-const nodeEnv = process.env.NODE_ENV;
-
-if (nodeEnv === "local" || nodeEnv === "test" || nodeEnv === "development") {
-  describe.skip("Initializes Financial Registration", () => {
-    context("Register", () => {
-      before(() => {
-        cy.visit(`${url}`, { timeout: 50000 }).then(() => {
-          cy.visit(`${url}create/registration`, { timeout: 50000 });
-        });
+describe("Initializes Financial Registration", () => {
+  context("Register", () => {
+    beforeEach(() => {
+      getCustomLogin("registrar");
+      getSingleRegistration("0-registration-blank");
+      postRegistrations("00-registration-newblank");
+      cy.visit(`${url}create/registration`, {
+        timeout: 50000,
       });
-      it("resets all info with the reset button and prevents submission with missing info", () => {
-        cy.get(".submission-form-buttons button").contains("Reset").click();
+      cy.location("pathname").should("include", "create");
+      cy.wait([
+        "@inituser",
+        "@getLogin",
+        "@getSettings",
+        "@getSingleRegistration",
+        "@postRegistrations",
+      ]);
+      getSingleRegistration("00-registration-newblank");
+      cy.location("pathname").should("include", "edit");
+
+      cy.wait(["@getSingleRegistration"]);
+    });
+    it("resets all info with the reset button and prevents submission with missing info", () => {
+      cy.get(".submission-form-buttons button").contains("Reset").click();
+      cy.get(".submission-form-buttons button").contains("Submit").click();
+      cy.get("#branch-help").should("exist");
+      cy.get("#branch-help").should("exist");
+      cy.get("#primarycontact-help").should("exist");
+      cy.get("#primaryemail-help").should("exist");
+      cy.get("#financialcontact-help").should("exist");
+      cy.get("#clientministry-help").should("exist");
+      cy.get("#respcode-help").should("exist");
+      cy.get("#serviceline-help").should("exist");
+      cy.get("#stob-help").should("exist");
+      cy.get("#project-help").should("exist");
+    });
+    it("prevents submission with incorrect info", () => {
+      cy.fixture("requests/financial-info").then((finances) => {
+        const { faultyOrg } = finances;
+        const testOrg = faultyOrg;
+
+        cy.get(
+          ".financial-registration-form #organization .p-dropdown-trigger"
+        ).click();
+
+        cy.get("#organization_list li")
+          .contains(testOrg["organization"])
+          .should("not.exist");
+
+        cy.get(
+          ".financial-registration-form #organization .p-dropdown-trigger"
+        ).click();
+
+        cy.get(".text-field #branch").type("{selectAll}", "{del}");
+        cy.get(".text-field #branch").invoke("val", "");
+
+        cy.get(".text-field #primarycontact").type("{selectAll}", "{del}");
+        cy.get(".text-field #primarycontact").invoke("val", "");
+
+        cy.get(".text-field #primaryemail").type("{selectAll}", "{del}");
+        cy.get(".text-field #primaryemail").invoke("val", "");
+
+        cy.get(".text-field #financialcontact").type("{selectAll}", "{del}");
+        cy.get(".text-field #financialcontact").invoke("val", "");
+
+        cy.get(".text-field #financialcontact").type("{selectAll}", "{del}");
+        cy.get(".text-field #financialcontact").invoke("val", "");
+
+        cy.get(".number-field #clientministry").type("{selectAll}", "{del}");
+        cy.get(".number-field #clientministry").type(testOrg["clientministry"]);
+        cy.get(".number-field #respcode").type("{selectAll}", "{del}");
+        cy.get(".number-field #respcode").type(testOrg["respcode"]);
+        cy.get(".number-field #serviceline").type("{selectAll}", "{del}");
+        cy.get(".number-field #serviceline").type(testOrg["serviceline"]);
+        cy.get(".number-field #stob").type("{selectAll}", "{del}");
+        cy.get(".number-field #stob").type(testOrg["stob"]);
+        cy.get(".number-field #project").type("{selectAll}", "{del}");
+        cy.get(".number-field #project").type(testOrg["project"]);
         cy.get(".submission-form-buttons button").contains("Submit").click();
+
         cy.get("#branch-help").should("exist");
         cy.get("#branch-help").should("exist");
         cy.get("#primarycontact-help").should("exist");
@@ -29,112 +98,56 @@ if (nodeEnv === "local" || nodeEnv === "test" || nodeEnv === "development") {
         cy.get("#stob-help").should("exist");
         cy.get("#project-help").should("exist");
       });
-      it("prevents submission with incorrect info", () => {
-        cy.fixture("financial-info").then((finances) => {
-          const { faultyOrg } = finances;
-          const testOrg = faultyOrg;
+    });
+    it("submits registration", () => {
+      cy.get(
+        ".financial-registration-form #organization .p-dropdown-trigger"
+      ).click();
 
-          cy.get(
-            ".financial-registration-form #organization .p-dropdown-trigger"
-          ).click();
+      cy.fixture("requests/financial-info").then((finances) => {
+        const { org1 } = finances;
+        let testOrg = org1;
 
-          cy.get("#organization_list li")
-            .contains(testOrg["organization"])
-            .should("not.exist");
+        cy.get("#organization_list li")
+          .contains(testOrg["organization"])
+          .click();
 
-          cy.get(
-            ".financial-registration-form #organization .p-dropdown-trigger"
-          ).click();
-
-          cy.get(".text-field #branch").type("{selectAll}", "{del}");
-          cy.get(".text-field #branch").invoke("val", "");
-
-          cy.get(".text-field #primarycontact").type("{selectAll}", "{del}");
-          cy.get(".text-field #primarycontact").invoke("val", "");
-
-          cy.get(".text-field #primaryemail").type("{selectAll}", "{del}");
-          cy.get(".text-field #primaryemail").invoke("val", "");
-
-          cy.get(".text-field #financialcontact").type("{selectAll}", "{del}");
-          cy.get(".text-field #financialcontact").invoke("val", "");
-
-          cy.get(".text-field #financialcontact").type("{selectAll}", "{del}");
-          cy.get(".text-field #financialcontact").invoke("val", "");
-
-          cy.get(".number-field #clientministry").type("{selectAll}", "{del}");
-          cy.get(".number-field #clientministry").type(
-            testOrg["clientministry"]
-          );
-          cy.get(".number-field #respcode").type("{selectAll}", "{del}");
-          cy.get(".number-field #respcode").type(testOrg["respcode"]);
-          cy.get(".number-field #serviceline").type("{selectAll}", "{del}");
-          cy.get(".number-field #serviceline").type(testOrg["serviceline"]);
-          cy.get(".number-field #stob").type("{selectAll}", "{del}");
-          cy.get(".number-field #stob").type(testOrg["stob"]);
-          cy.get(".number-field #project").type("{selectAll}", "{del}");
-          cy.get(".number-field #project").type(testOrg["project"]);
-          cy.get(".submission-form-buttons button").contains("Submit").click();
-
-          cy.get("#branch-help").should("exist");
-          cy.get("#branch-help").should("exist");
-          cy.get("#primarycontact-help").should("exist");
-          cy.get("#primaryemail-help").should("exist");
-          cy.get("#financialcontact-help").should("exist");
-          cy.get("#clientministry-help").should("exist");
-          cy.get("#respcode-help").should("exist");
-          cy.get("#serviceline-help").should("exist");
-          cy.get("#stob-help").should("exist");
-          cy.get("#project-help").should("exist");
-        });
-      });
-      it("submits registration", () => {
         cy.get(
-          ".financial-registration-form #organization .p-dropdown-trigger"
-        ).click();
+          ".financial-registration-form #organization .p-inputtext"
+        ).should("contain", testOrg["organization"]);
 
-        cy.fixture("financial-info").then((finances) => {
-          const { org1 } = finances;
-          let testOrg = org1;
+        cy.get(".text-field #branch").type("{selectAll}", "{del}");
+        cy.get(".text-field #branch").type(testOrg["branch"]);
 
-          cy.get("#organization_list li")
-            .contains(testOrg["organization"])
-            .click();
+        cy.get(".text-field #primarycontact").type("{selectAll}", "{del}");
+        cy.get(".text-field #primarycontact").type(testOrg["primarycontact"]);
 
-          cy.get(
-            ".financial-registration-form #organization .p-inputtext"
-          ).should("contain", testOrg["organization"]);
+        cy.get(".text-field #primaryemail").type("{selectAll}", "{del}");
+        cy.get(".text-field #primaryemail").type(testOrg["primaryemail"]);
 
-          cy.get(".text-field #branch").type("{selectAll}", "{del}");
-          cy.get(".text-field #branch").type(testOrg["branch"]);
+        cy.get(".text-field #financialcontact").type("{selectAll}", "{del}");
+        cy.get(".text-field #financialcontact").type(
+          testOrg["financialcontact"]
+        );
 
-          cy.get(".text-field #primarycontact").type("{selectAll}", "{del}");
-          cy.get(".text-field #primarycontact").type(testOrg["primarycontact"]);
+        cy.get(".number-field #clientministry").type("{selectAll}", "{del}");
+        cy.get(".number-field #clientministry").type(testOrg["clientministry"]);
+        cy.get(".number-field #respcode").type("{selectAll}", "{del}");
+        cy.get(".number-field #respcode").type(testOrg["respcode"]);
+        cy.get(".number-field #serviceline").type("{selectAll}", "{del}");
+        cy.get(".number-field #serviceline").type(testOrg["serviceline"]);
+        cy.get(".number-field #stob").type("{selectAll}", "{del}");
+        cy.get(".number-field #stob").type(testOrg["stob"]);
+        cy.get(".number-field #project").type("{selectAll}", "{del}");
+        cy.get(".number-field #project").type(testOrg["project"]);
 
-          cy.get(".text-field #primaryemail").type("{selectAll}", "{del}");
-          cy.get(".text-field #primaryemail").type(testOrg["primaryemail"]);
+        //Add New registration
+        postSingleRegistration("0-registration-new");
 
-          cy.get(".text-field #financialcontact").type("{selectAll}", "{del}");
-          cy.get(".text-field #financialcontact").type(
-            testOrg["financialcontact"]
-          );
-
-          cy.get(".number-field #clientministry").type("{selectAll}", "{del}");
-          cy.get(".number-field #clientministry").type(
-            testOrg["clientministry"]
-          );
-          cy.get(".number-field #respcode").type("{selectAll}", "{del}");
-          cy.get(".number-field #respcode").type(testOrg["respcode"]);
-          cy.get(".number-field #serviceline").type("{selectAll}", "{del}");
-          cy.get(".number-field #serviceline").type(testOrg["serviceline"]);
-          cy.get(".number-field #stob").type("{selectAll}", "{del}");
-          cy.get(".number-field #stob").type(testOrg["stob"]);
-          cy.get(".number-field #project").type("{selectAll}", "{del}");
-          cy.get(".number-field #project").type(testOrg["project"]);
-
-          cy.get(".submission-form-buttons button").contains("Submit").click();
-          cy.location("pathname").should("include", "registration");
-        });
+        cy.get(".submission-form-buttons button").contains("Submit").click();
+        cy.wait(["@postSingleRegistration"]);
+        cy.location("pathname").should("include", "registration");
       });
     });
   });
-}
+});
--- cypress/e2e/custom-events-testing/registration-flow/2-registration-page.cy.js
+++ cypress/e2e/custom-events-testing/registration-flow/2-registration-page.cy.js
@@ -1,16 +1,27 @@
 /// <reference types="cypress" />
 const url = Cypress.env("url");
-const user = Cypress.env("user");
+import { getCustomLogin } from "../../helpers/login";
+import {
+  getRegistrations,
+  getSingleRegistration,
+  postRegistrations,
+} from "../../helpers/registrations";
 
-describe.skip("Registration Navigation Process", () => {
+describe("Registration Navigation Process", () => {
   context("Registration Page for registrar", () => {
     beforeEach(() => {
+      getCustomLogin();
+      getRegistrations();
+      getSingleRegistration();
+      postRegistrations();
       cy.visit(url, { timeout: 50000 });
+      cy.wait(["@inituser", "@getLogin", "@getSettings"]);
     });
 
     it("navigates to the registration page when clicking on the register button", () => {
       Cypress.$("document").ready(function () {
         cy.get(".p-button").contains("Register").click();
+        cy.wait(["@getRegistrations", "@getSingleRegistration"]);
         cy.location("pathname").should("include", "registration");
       });
     });
@@ -19,40 +30,9 @@ describe.skip("Registration Navigation Process", () => {
       Cypress.$("document").ready(function () {
         cy.get(".dropdown-account").click();
         cy.get(".dropdown-account").contains("My Registration").click();
+        cy.wait(["@getRegistrations", "@getSingleRegistration"]);
         cy.location("pathname").should("include", "registration");
       });
-    });
-  });
-});
-
-describe.skip("Registration Functional and Displays Information", () => {
-  context("Page Buttons", () => {
-    beforeEach(() => {
-      cy.visit(`${url}create/registration`);
-    });
-
-    it("opens and closes the edit pop-up for financial information", () => {
-      cy.get("#personal-registration-table .edit-button")
-        .contains("Edit")
-        .click();
-      cy.get(".financial-registration-form")
-        .contains("Organizational Information")
-        .should("be.visible");
-
-      cy.get(".registration-dialog .p-dialog-header-close").click();
-      cy.get(".financial-registration-form").should("not.exist");
-    });
-
-    it("opens and closes the delete confirmation pop-up", () => {
-      cy.get("#personal-registration-table .delete-button")
-        .contains("Delete")
-        .click();
-      cy.get(".confirmation-content")
-        .contains("Are you sure you want to delete")
-        .should("be.visible");
-
-      cy.get(".p-button").contains("No").click();
-      cy.get(".confirmation-content").should("not.exist");
     });
   });
 });
