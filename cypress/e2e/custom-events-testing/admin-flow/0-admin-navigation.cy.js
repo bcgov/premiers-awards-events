0233f877a6c48c5f52e25267b6a0aebcf59060dd
BrysonBest <103070659+brysonjbest@users.noreply.github.com>
2022-11-17 09:49:59 -0800

    New cypress version new testing requirements.

--- cypress/e2e/custom-events-testing/admin-flow/0-admin-navigation.cy.js
+++ cypress/e2e/custom-events-testing/admin-flow/0-admin-navigation.cy.js
@@ -7,78 +7,64 @@ describe("Admin Navigation Process", () => {
   });
   context("Registration Page for admin", () => {
     it("navigates to the registration page when clicking on the register button", () => {
-      Cypress.$("document").ready(function () {
-        cy.get(".p-button").contains("Register").click();
-        cy.location("pathname").should("include", "registration");
-      });
+      cy.get(".p-button").contains("Register").click();
+      cy.location("pathname").should("include", "registration");
     });
+  });
 
+  context("Registration page from dropdown for admin", () => {
     it("navigates to the registration page when clicking on the register dropdown", () => {
-      Cypress.$("document").ready(function () {
-        cy.get(".dropdown-account").click();
-        cy.get(".dropdown-account").contains("My Registration").click();
-        cy.location("pathname").should("include", "registration");
-      });
+      cy.get(".dropdown-account").click();
+      cy.get(".dropdown-account").contains("My Registration").click();
+      cy.location("pathname").should("include", "registration");
     });
   });
 
   context("View Profile Page for admin", () => {
     it("navigates to the view profile page from the dropdown", () => {
-      Cypress.$("document").ready(function () {
-        cy.get(".dropdown-account").click();
-        cy.get(".dropdown-account").contains("View Profile").click();
-        cy.location("pathname").should("include", "user/update");
-      });
+      cy.get(".dropdown-account").click();
+      cy.get(".dropdown-account").contains("View Profile").click();
+      cy.location("pathname").should("include", "user/update");
     });
   });
 
   context("View Registrations Page for admin", () => {
     it("navigates to the view registrations page from the dropdown", () => {
-      Cypress.$("document").ready(function () {
-        cy.get(".dropdown-account").click();
-        cy.get(".dropdown-account").contains("View Registrations").click();
-        cy.location("pathname").should("include", "admin");
-      });
+      cy.get(".dropdown-account").click();
+      cy.get(".dropdown-account").contains("View Registrations").click();
+      cy.location("pathname").should("include", "admin");
     });
   });
 
   context("View Guests Page for admin", () => {
     it("navigates to the view guests page from the dropdown", () => {
-      Cypress.$("document").ready(function () {
-        cy.get(".dropdown-account").click();
-        cy.get(".dropdown-account").contains("View Guests").click();
-        cy.location("pathname").should("include", "admin/guests");
-      });
+      cy.get(".dropdown-account").click();
+      cy.get(".dropdown-account").contains("View Guests").click();
+      cy.location("pathname").should("include", "admin/guests");
     });
   });
 
   context("View Tables Page for admin", () => {
     it("navigates to the view tables page from the dropdown", () => {
-      Cypress.$("document").ready(function () {
-        cy.get(".dropdown-account").click();
-        cy.get(".dropdown-account").contains("View Tables").click();
-        cy.location("pathname").should("include", "admin/tables");
-      });
+      cy.get(".dropdown-account").click();
+      cy.get(".dropdown-account").contains("View Tables").click();
+      cy.location("pathname").should("include", "admin/tables");
     });
   });
 
   context("Event Planner Page for admin", () => {
     it("navigates to the Event Planning page from the dropdown", () => {
-      Cypress.$("document").ready(function () {
-        cy.get(".dropdown-account").click();
-        cy.get(".dropdown-account").contains("Event Planner").click();
-        cy.location("pathname").should("include", "tables/event/planning");
-      });
+      cy.get(".dropdown-account").click();
+      cy.get(".dropdown-account").contains("Event Planner").click();
+      cy.location("pathname").should("include", "tables/event/planning");
     });
   });
 
   context("Event Settings Page for admin", () => {
     it("navigates to the event settings page from the dropdown", () => {
-      Cypress.$("document").ready(function () {
-        cy.get(".dropdown-account").click();
-        cy.get(".dropdown-account").contains("Event Settings").click();
-        cy.location("pathname").should("include", "settings");
-      });
+      cy.get(".dropdown-account").click();
+      cy.get(".dropdown-account").contains("Event Settings").click();
+      cy.location("pathname").should("include", "settings");
     });
   });
 });
