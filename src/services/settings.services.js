b7b8859f9a83863e743e6bc3db31aedb3522fe8a
DenisKorkut-bcgov <125493524+DenisKorkut-bcgov@users.noreply.github.com>
2023-10-24 14:14:50 -0700

    Reverted org ordering / matched to admin app

--- src/services/settings.services.js
+++ src/services/settings.services.js
@@ -63,72 +63,64 @@ const schemaData = {
     { value: "super-administrator", text: "Super-Administrator" },
   ],
   organizations: [
-    { value: "org-1", text: "BC Public Service Agency" },
-    { value: "org-2", text: "Environmental Assessment Office" },
     {
-      value: "org-3",
-      text: "Government Communications and Public Engagement",
-    },
-    {
-      value: "org-4",
+      value: "org-1",
       text: "Ministry of Post-Secondary Education and Future Skills",
     },
-    { value: "org-5", text: "Ministry of Agriculture and Food" },
+    { value: "org-2", text: "Ministry of Agriculture and Food" },
+    { value: "org-3", text: "Ministry of Attorney General" },
+    { value: "org-4", text: "Ministry of Children and Family Development" },
+    { value: "org-5", text: "Ministry of Citizens’ Services" },
+    { value: "org-6", text: "Ministry of Education and Child Care" },
     {
-      value: "org-6",
-      text: "Ministry of Attorney General",
-    },
-    { value: "org-7", text: "Ministry of Children and Family Development" },
-    { value: "org-8", text: "Ministry of Citizens’ Services" },
-    { value: "org-9", text: "Ministry of Education and Child Care" },
-    {
-      value: "org-10",
-      text: "Ministry of Energy, Mines and Low Carbon Innovation",
-    },
-    {
-      value: "org-11",
-      text: "Ministry of Environment and Climate Change Strategy",
-    },
-    { value: "org-12", text: "Ministry of Finance" },
-    {
-      value: "org-13",
-      text: "Ministry of Forests",
-    },
-    { value: "org-14", text: "Ministry of Health" },
-    {
-      value: "org-15",
-      text: "Ministry of Indigenous Relations and Reconciliation",
-    },
-    {
-      value: "org-16",
-      text: "Ministry of Ministry of Jobs, Economic Development and Innovation",
-    },
-    { value: "org-17", text: "Ministry of Labour" },
-    {
-      value: "org-18",
-      text: "Water, Land and Resource Stewardship",
-    },
-    { value: "org-19", text: "Ministry of Mental Health and Addictions" },
-    { value: "org-20", text: "Ministry of Municipal Affairs" },
-    {
-      value: "org-21",
-      text: "Ministry of Public Safety and Solicitor General",
-    },
-    {
-      value: "org-22",
-      text: "Ministry of Social Development & Poverty Reduction",
-    },
-    { value: "org-23", text: "Ministry of Tourism, Arts, Culture and Sport" },
-    { value: "org-24", text: "Ministry of Transportation & Infrastructure" },
-    { value: "org-25", text: "Office of the Premier" },
-    {
-      value: "org-26",
+      value: "org-7",
       text: "Ministry of Emergency Management and Climate Readiness",
     },
     {
-      value: "org-27",
-      text: "Ministry of Housing",
+      value: "org-8",
+      text: "Ministry of Energy, Mines and Low Carbon Innovation",
     },
+    {
+      value: "org-9",
+      text: "Ministry of Environment and Climate Change Strategy",
+    },
+    { value: "org-10", text: "Environmental Assessment Office" },
+    { value: "org-11", text: "Ministry of Finance" },
+    {
+      value: "org-12",
+      text: "Ministry of Forests",
+    },
+    { value: "org-13", text: "Ministry of Health" },
+    {
+      value: "org-14",
+      text: "Ministry of Indigenous Relations and Reconciliation",
+    },
+    {
+      value: "org-15",
+      text: "Ministry of Jobs, Economic Development and Innovation",
+    },
+    { value: "org-16", text: "Ministry of Labour" },
+    { value: "org-17", text: "Ministry of Mental Health and Addictions" },
+    { value: "org-18", text: "Ministry of Municipal Affairs" },
+
+    {
+      value: "org-19",
+      text: "Ministry of Public Safety and Solicitor General",
+    },
+    {
+      value: "org-20",
+      text: "Ministry of Social Development & Poverty Reduction",
+    },
+    { value: "org-21", text: "Ministry of Tourism, Arts, Culture and Sport" },
+    { value: "org-22", text: "Ministry of Transportation & Infrastructure" },
+    { value: "org-23", text: "BC Public Service Agency" },
+    {
+      value: "org-24",
+      text: "Government Communications and Public Engagement",
+    },
+    { value: "org-25", text: "Office of the Premier" },
+    { value: "org-26", text: "Water, Land and Resource Stewardship" },
+    { value: "org-27", text: "Ministry of Housing" },
     {
       value: "org-28",
       text: "Declaration Act Secretariat",
