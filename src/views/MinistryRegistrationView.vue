6436b4850515446c0e2419a8d3af0763ae1046d8
DenisKorkut-bcgov <125493524+DenisKorkut-bcgov@users.noreply.github.com>
2023-11-14 09:29:43 -0800

    Cleanup Ministry Registration

--- src/views/MinistryRegistrationView.vue
+++ src/views/MinistryRegistrationView.vue
@@ -4,12 +4,8 @@ import { useFinancialStore } from "../stores/financial";
 import { ref } from "vue";
 import PageHeader from "../components/common/PageHeader.vue";
 import RegistrationList from "../components/RegistrationList.vue";
-import formServices from "../services/settings.services";
-import NavMenu from "../components/common/NavMenu.vue";
 const userStore = useAuthUserStore();
 const financialStore = useFinancialStore();
-
-const navItems = ref(formServices.get("navItems") || []);
 
 const tableInfoDialog = ref(false);
 const tableCountAll = () => {
