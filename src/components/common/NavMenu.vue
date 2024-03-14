05068f79b2c67dbc88fc55cd8cd3521bafccd44c
brysonjbest <103070659+brysonjbest@users.noreply.github.com>
2024-03-14 08:43:46 -0700

    Update menu navigation.

--- src/components/common/NavMenu.vue
+++ src/components/common/NavMenu.vue
@@ -6,26 +6,50 @@
       <div class="spacer" v-else></div>
       <TabMenu
         class="admin-nav"
-        :model="menuitems"
-        :activeIndex="activeIndex"
-      />
+        :model="activeMenuitems"
+        :activeIndex="active"
+      ><template #item="{ item, props }">
+                <router-link v-slot="{ href, navigate }" :to="item.to" custom>
+                    <a :href="href" v-bind="props.action" @click="navigate">
+                        <span v-bind="props.icon" />
+                        <span v-bind="props.label">{{ item.label }}</span>
+                    </a>
+                </router-link>
+            </template></TabMenu>
       <RouterView />
     </div>
   </div>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, watch } from "vue";
+import { useRoute } from "vue-router";
 
 export default {
   props: {
     title: String,
     menuitems: Array,
   },
-  setup() {
-    const activeIndex = ref(0);
+  setup(props) {
+    const route = useRoute();
+    const active = ref(0);
 
-    return { activeIndex };
+    const indexedMenuItems = props.menuitems.map((obj, index) => {
+        return { ...obj, index: index + 1 };
+    });
+    const activeMenuitems = ref(indexedMenuItems)
+
+    //check current path and update reference location of active
+    const updateActive = (items) => {
+    const index = items.findIndex(item => item.to === route.path);
+    if (index !== -1) {
+        active.value = items[index].index - 1;
+    }
+    };
+
+    updateActive(indexedMenuItems);
+
+    return { activeMenuitems, active };
   },
 };
 </script>
