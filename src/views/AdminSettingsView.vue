7c86b52752f13a2dd3c7ad1f4338a32e36e7e1fb
DenisKorkut-bcgov <125493524+DenisKorkut-bcgov@users.noreply.github.com>
2024-03-26 11:05:08 -0700

    Allow editing Sales Date Settings by admins

--- src/views/AdminSettingsView.vue
+++ src/views/AdminSettingsView.vue
@@ -237,7 +237,7 @@ settingsStore.fillSettings().then(() => {
           </ol>
         </template>
       </PrimeCard>
-      <PrimeCard id="eventDateSettings" v-if="userStore.isSuperAdmin">
+      <PrimeCard id="eventDateSettings" v-if="userStore.isAdmin">
         <template #subtitle>
           <h1>Event Date Settings</h1>
         </template>
