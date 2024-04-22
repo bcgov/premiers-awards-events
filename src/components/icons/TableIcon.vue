e0325a83a2026d4cb23a7527dfefa3b169593deb
DenisKorkut-bcgov <125493524+DenisKorkut-bcgov@users.noreply.github.com>
2024-04-22 11:27:03 -0700

    PA-108 Update thickness for reserved tables in table organizer

--- src/components/icons/TableIcon.vue
+++ src/components/icons/TableIcon.vue
@@ -2,12 +2,18 @@
 <template>
   <div class="table-icon">
     <div class="p-1">
-      <SpeedDial :model="tableGuests" :radius="radiusCalc()" type="circle" :buttonClass="tableClass"
-        @click="getGuests()" :tooltipOptions="{ position: 'top' }"
-        :showIcon="tableGuests.length > 0 ? 'pi pi-users' : 'pi'" hideIcon="pi pi-times"
-        :disabled="tableGuests.length <= 0" />
+      <SpeedDial
+        :model="tableGuests"
+        :radius="radiusCalc()"
+        type="circle"
+        :buttonClass="tableClass"
+        @click="getGuests()"
+        :tooltipOptions="{ position: 'top' }"
+        :showIcon="tableGuests.length > 0 ? 'pi pi-users' : 'pi'"
+        hideIcon="pi pi-times"
+        :disabled="tableGuests.length <= 0"
+      />
     </div>
-
   </div>
 </template>
 
@@ -26,7 +32,7 @@ export default {
       "full-table": `${defaultButton} full-table`,
       "half-table": `${defaultButton} half-table`,
       "empty-table": `${defaultButton} empty-table`,
-      default: `${defaultButton} default-table`
+      default: `${defaultButton} default-table`,
     };
 
     const tableClass = computed(() => {
@@ -37,39 +43,38 @@ export default {
       return statusClass;
     });
 
-
     // Load guest data to each table icon on request
     let tableGuests = ref(table.guests);
     const getGuests = async () => {
-      const newGuests = (await apiRoutesTables.getGuestsByTable(table.guid));
+      const newGuests = await apiRoutesTables.getGuestsByTable(table.guid);
       const newGuestData = newGuests.data[0].guests || null;
       if (newGuests) {
-        tableGuests.value = newGuestData.map(each => ({
+        tableGuests.value = newGuestData.map((each) => ({
           ...each,
           label: `${each.seat}\n${each.firstname}\n${each.lastname}`,
-          icon: 'pi pi-user',
+          icon: "pi pi-user",
           command: () => {
             router.push(`/admin/edit/${each.registration}`);
-          }
+          },
         }));
       }
-    }
+    };
 
     //Adjust radius when more than 10 guests seated at a table
     const radiusCalc = () => {
       let radius = 90;
       const guestCount = tableGuests.value.length;
       if (guestCount > 10) {
-        radius += ((guestCount - 10) * 4)
+        radius += (guestCount - 10) * 4;
       }
-      return radius
-    }
+      return radius;
+    };
 
     return {
       tableClass,
       tableGuests,
       getGuests,
-      radiusCalc
+      radiusCalc,
     };
   },
 };
@@ -89,23 +94,51 @@ export default {
 
   .half-table {
     /* For modern browsers */
-    background: linear-gradient(to right, red 0%, red 50%, green 50%, green 100%);
+    background: linear-gradient(
+      to right,
+      red 0%,
+      red 50%,
+      green 50%,
+      green 100%
+    );
 
     /* For older browsers */
-    background: -webkit-linear-gradient(left, red 0%, red 50%, green 50%, green 100%);
-    background: -moz-linear-gradient(left, red 0%, red 50%, green 50%, green 100%);
-    background: -o-linear-gradient(left, red 0%, red 50%, green 50%, green 100%);
-    background: -ms-linear-gradient(left, red 0%, red 50%, green 50%, green 100%);
+    background: -webkit-linear-gradient(
+      left,
+      red 0%,
+      red 50%,
+      green 50%,
+      green 100%
+    );
+    background: -moz-linear-gradient(
+      left,
+      red 0%,
+      red 50%,
+      green 50%,
+      green 100%
+    );
+    background: -o-linear-gradient(
+      left,
+      red 0%,
+      red 50%,
+      green 50%,
+      green 100%
+    );
+    background: -ms-linear-gradient(
+      left,
+      red 0%,
+      red 50%,
+      green 50%,
+      green 100%
+    );
   }
 
   .empty-table {
     background-color: $emptyColor;
-
   }
 
   .default-table {
     background-color: $defaultColor;
-
   }
 
   .p-speeddial {
@@ -114,7 +147,7 @@ export default {
     .reserved-table {
       border: solid;
       border-color: $reservedColor !important;
-
+      border-width: thick;
     }
 
     .p-button:disabled {
